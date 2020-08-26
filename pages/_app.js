97dc868695ded53aa42b14a00c72c772ec361e82
Peter Lau <superpeterlau@outlook.com>
2020-08-26 17:32:13 +0800

    test tailwindcss & emotion

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import '../styles/index.css'
+
+export default function RootApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
+import {css, jsx} from '@emotion/core'
+
+const color = 'white'
+
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
@@ -9,6 +13,17 @@ const IndexPage = () => (
         <a>About</a>
       </Link>
     </p>
+    <div
+    css={css`
+    padding: 32px;
+    background-color: hotpink;
+    font-size: 24px;
+    border-radius: 4px;
+    &:hover {
+      color: ${color};
+    }
+    `}
+    >Hover will change color</div>
   </Layout>
 )
 
